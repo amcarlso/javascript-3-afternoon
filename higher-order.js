c05a473fff4f8458ad604cbca59e2f5cfd810e14
Allen Carlson <allen.carlson123@gmail.com>
2018-11-28 20:41:01 -0700

    1-4 and 6 completed, attempt on 5

--- higher-order.js
+++ higher-order.js
@@ -98,7 +98,11 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter((el, i, arr)=> {
+  if(el.CP > 200) {
+    return el
+  }
+})
 
 
 
@@ -115,7 +119,15 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax. Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+// let itemPriceTotal = orders.reduce((acc, el)=> {
+//   return acc += el.price
+// }, 0)
+// let taxTotal = orders.reduce((acc, el)=> {
+//   return acc += el.tax
+// }, 0)
+let orderTotals = orders.map((el, i, arr)=> {
+  return el.price + el.tax
+})// Code here
 
 
 
